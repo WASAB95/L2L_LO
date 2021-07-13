50359469e9328778613dacc68ec25c31472d78ba
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2021-07-13 11:20:08 +0200

    added nest optimizee and network & netlogo model

--- l2l/optimizees/neuroevolution/__init__.py
+++ l2l/optimizees/neuroevolution/__init__.py
@@ -1,8 +1,10 @@
 from .optimizee import NeuroEvolutionOptimizee, NeuroEvolutionOptimizeeParameters
 from .optimizee_ant import NeuroEvolutionOptimizeeAnt, NeuroEvolutionOptimizeeAntParameters
 from .optimizee_ac import AntColonyOptimizee, AntColonyOptimizeeParameters
+from .optimizee_nest import NestOptimizee, NestOptimizeeParameters
 
 __all__ = ['NeuroEvolutionOptimizee', 'NeuroEvolutionOptimizeeParameters',
            'AntColonyOptimizee', 'AntColonyOptimizeeParameters',
-           'NeuroEvolutionOptimizeeAnt', 'NeuroEvolutionOptimizeeAntParameters'
+           'NeuroEvolutionOptimizeeAnt', 'NeuroEvolutionOptimizeeAntParameters',
+           'NestOptimizee', 'NestOptimizeeParameters'
            ]
