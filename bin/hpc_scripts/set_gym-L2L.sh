a921d65aeee9020e2bc84a49eff3ff1c0a7fa5fe
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2022-03-09 14:42:50 +0100

    added open ai gym script

---
+++ bin/hpc_scripts/set_gym-L2L.sh
@@ -0,0 +1,12 @@
+module load GCC/9.3.0
+module load OpenMPI/4.1.0rc1
+module load mpi4py/3.0.3-Python-3.8.5
+module load SciPy-Stack/2020-Python-3.8.5
+module load CMake/3.18.0
+module load GCCcore/.9.3.0
+module load scikit/2020-Python-3.8.5
+module load Java/15.0.1
+module load PyTorch
+module load OpenAI-Gym/0.18.0-Python-3.8.5
+
+export PYTHONPATH=$PYTHONPATH:<path_to_L2L>/:<path_to_sdict>/:<path_to_jube>/<path_to_nest_build>/install/lib64/python3.8/site-packages
